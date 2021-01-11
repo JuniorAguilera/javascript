63f4ee4b8bf22c62f077214ea6ebb474a94452b0
Junior Aguilera <junior.aguilera.alvarado@gmail.com>
2021-01-10 19:37:56 -0500

    creacion de funciones

---
+++ funciones.js
@@ -0,0 +1,24 @@
+//Declarativas
+
+function miFuncion(){
+    return 3;
+}
+
+//Expresion
+
+var miFuncion = function(a,b){
+    return a + b;
+}
+
+miFuncion(3,4);
+
+
+var miNuevaFuncion = function(d,e){
+    return d*e;
+}
+
+miNuevaFuncion(3,4);
+
+var saludarEstudiante (estudiante){
+    return "Hola " + estudiante;
+}
